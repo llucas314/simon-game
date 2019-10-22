be37034fdcea11928b41136704fcaf0ec96405f9
Ren Lucas <llucas314@gmail.com>
2019-10-22 11:59:52 -0400

    colored buttons

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const redBtn = document.querySelector('#red');
-const blueBtn = document.querySelector('#blue');
-const greenBtn = document.querySelector('#green');
-const yellowBtn = document.querySelector('#yellow');
+const redBtn = document.querySelector('.red');
+const blueBtn = document.querySelector('.blue');
+const greenBtn = document.querySelector('.green');
+const yellowBtn = document.querySelector('.yellow');
 const easyBtn = document.querySelector('#easy');
 const mediumBtn = document.querySelector('#medium');
 const hardBtn = document.querySelector('#hard');
 const expertBtn = document.querySelector('#expert');
 const section = document.querySelector('section')
 const main = document.querySelector('main')
+
+const order = [];
+const 
